2e4a6df680c7368721467ab9e0d98d473ba952fb
Ivan Proskuryakov <axzhbi5wcm9za3vyewfrb3y@gmail.com>
2023-04-12 22:03:26 +0300

    learn: learning basics qt

--- main.cpp
+++ main.cpp
@@ -6,12 +6,12 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
 
-    w.setWindowIcon(QIcon("logo.png"));
+    QIcon icon("/Users/ivan/code/communa/CommunaTimeTracker/resources/logo.png");
+
+    a.setWindowIcon(icon);
+
     w.setWindowTitle("Communa / TimeTracker");
     w.show();
-
-    w.show();
-
 
     return a.exec();
 }
